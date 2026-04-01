d62fe6a2c464bcad23488a4b0c12fc2087752188
sarki012 <erik.sarkinen@gmail.com>
2026-04-01 10:08:38 -0400

    Init.

---
+++ src/neural_networks/utils.py
@@ -0,0 +1,17 @@
+# EE 5271 Robot Vision Homework 3 Deep Learning
+# Erik Sarkinen
+# 3854563
+# 4/1/26
+
+from torch.utils.data import DataLoader, TensorDataset
+
+# Create datasets
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
+# Create data loaders
+train_loader = DataLoader(train_dataset,
+batch_size=32,
+shuffle=True)
+test_loader = DataLoader(test_dataset,
+batch_size=32,
+shuffle=False)
