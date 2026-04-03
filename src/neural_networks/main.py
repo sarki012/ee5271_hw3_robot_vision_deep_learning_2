e0e23c17b9b42a4479a47e03ecf26290deb9c1fa
sarki012 <erik.sarkinen@gmail.com>
2026-04-03 16:40:25 -0400

    MLP Acc. 93-94% Looks Good.

--- src/neural_networks/main.py
+++ src/neural_networks/main.py
@@ -13,12 +13,13 @@ import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 import torch.optim as optim
-from models import SLPLinear
+from models import SLPLinear, MLP
 from torch.optim.lr_scheduler import StepLR
 from data_utils import get_train_loader
 import train
 
 def main():
+    '''
     # Instantiate the class
     model = SLPLinear(input_size=196, num_classes=10)
     optimizer = optim.SGD(model.parameters(), lr=0.05)
@@ -28,6 +29,18 @@ def main():
     scheduler = StepLR(optimizer, step_size=10, gamma=0.9)
     num_epochs = 30
     criterion = nn.MSELoss()
+    train_loader = get_train_loader()
+    train.train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs)
+'''
+    # Instantiate the class
+    model = MLP(input_size=196, num_classes=10)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    # 2. Define the StepLR scheduler
+    # step_size: how many epochs to wait before reducing the LR (10)
+    # gamma: the multiplier for the LR (0.9)
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.95)
+    num_epochs = 30
+    criterion = nn.CrossEntropyLoss()
     train_loader = get_train_loader()
     train.train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs)
 
--- src/neural_networks/models.py
+++ src/neural_networks/models.py
@@ -20,6 +20,17 @@ class SLPLinear(nn.Module):
     def forward(self, x):
         return self.fc(x)
     
+class MLP(nn.Module):
+    def __init__(self, input_size=196, hidden_size=30,num_classes=10):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
     
     
     
